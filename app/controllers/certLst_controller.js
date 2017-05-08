8d75073362bf4e9d69c8750b36e24818b0086694
Vishwas Anand Bhushan <vishwasanand.cse@gmail.com>
2017-05-08 08:10:59 +0530

    Some code cleanup and duplicate issue fixed.

--- app/controllers/certLst_controller.js
+++ app/controllers/certLst_controller.js
@@ -7,13 +7,6 @@ serviceController.getCerts = function (req, res, next) {
   dbconnection
     .authenticate()
     .then(function(err) {
-<<<<<<< HEAD
-=======
-      // console.log('Connection has been established successfully.');
-      // console.log('==========================Request Object =======================================================');
-      // console.log(req);
-      // console.log('================================================================================================');
->>>>>>> 8c4b6e51d2069fb1a7bbcd5ce6fc34d9948bfd0e
       CertificateList_Model.findAll().then(function(data){
         res.json(data);
       });
@@ -27,21 +20,6 @@ serviceController.postCerts = function (req, res, next) {
   dbconnection
     .authenticate()
     .then(function(err) {
-      // console.log('------------------------CertificateList_Model -------------------');
-      // console.log(CertificateList_Model);
-      // console.log('------------------------CertificateList_Model -------------------');
-      console.log('------------------------req Object -------------------');
-      console.log(req.body);
-      console.log('------------------------req Object -------------------');
-
-//       var body = [];
-// req.on('data', function(chunk) {
-//   body.push(chunk);
-// }).on('end', function() {
-//   body = Buffer.concat(body).toString();
-//   // at this point, `body` has the entire request body stored in it as a string
-// });
-// console.log(body);
         CertificateList_Model.update({
           CryptoObject : 'CryptoObject_updated',
           NotBefore : 'NotBefore_updated',
